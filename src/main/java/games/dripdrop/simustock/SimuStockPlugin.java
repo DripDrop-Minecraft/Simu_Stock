192afe1054015c3a5f1688a48fdaf8ab0ad4e14c
mac <DripDropMC@outlook.com>
2024-07-09 21:36:16 +0800

    feat(all): init commit

---
+++ src/main/java/games/dripdrop/simustock/SimuStockPlugin.java
@@ -0,0 +1,17 @@
+package games.dripdrop.simustock;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimuStockPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
