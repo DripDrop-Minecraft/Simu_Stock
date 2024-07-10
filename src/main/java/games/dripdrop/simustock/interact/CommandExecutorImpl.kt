ff646e4ac6efecb92686ad681b3dfa69e8816926
mac <DripDropMC@outlook.com>
2024-07-10 23:43:33 +0800

    feat(all): add command executor

---
+++ src/main/java/games/dripdrop/simustock/interact/CommandExecutorImpl.kt
@@ -0,0 +1,23 @@
+package games.dripdrop.simustock.interact
+
+import games.dripdrop.simustock.interfaces.ICommand
+import org.bukkit.command.CommandSender
+
+internal class CommandExecutorImpl : ICommand {
+    override fun execute(sender: CommandSender, args: Array<out String>?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getTabCommandList(sender: CommandSender, args: Array<out String>?): MutableList<String> {
+        TODO("Not yet implemented")
+    }
+
+    override fun help() {
+        TODO("Not yet implemented")
+    }
+
+    override fun reloadConfig() {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ src/main/java/games/dripdrop/simustock/interact/CommandManager.kt
@@ -0,0 +1,34 @@
+package games.dripdrop.simustock.interact
+
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.command.TabExecutor
+
+const val commandName = "stock"
+const val commandAlias = "stk"
+
+class CommandManager: CommandExecutor, TabExecutor {
+    private val mExecutor = CommandExecutorImpl()
+
+    override fun onTabComplete(
+        sender: CommandSender,
+        command: Command,
+        label: String,
+        args: Array<out String>?
+    ): MutableList<String> = mExecutor.getTabCommandList(sender, args)
+
+    override fun onCommand(
+        sender: CommandSender,
+        command: Command,
+        alias: String,
+        args: Array<out String>?
+    ): Boolean {
+        return if (command.name.equals(commandName, true) || alias.equals(commandAlias, true)) {
+            mExecutor.execute(sender, args)
+            true
+        } else {
+            false
+        }
+    }
+}
