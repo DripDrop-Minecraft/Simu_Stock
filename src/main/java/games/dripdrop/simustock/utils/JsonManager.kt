09c80b1e7779c3a7c38cbd8f784bafb4d42ab540
mac <DripDropMC@outlook.com>
2024-07-12 20:17:58 +0800

    refactor(utils): add or change some apis

--- src/main/java/games/dripdrop/simustock/utils/JsonManager.kt
+++ src/main/java/games/dripdrop/simustock/utils/JsonManager.kt
@@ -2,10 +2,14 @@ package games.dripdrop.simustock.utils
 
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+import games.dripdrop.simustock.bean.PluginFile
 import java.io.BufferedReader
+import java.io.File
 import java.io.FileInputStream
 import java.io.InputStreamReader
 import java.nio.charset.Charset
+import java.nio.file.Files
 
 object JsonManager {
     val mGson: Gson = GsonBuilder()
@@ -15,9 +19,30 @@ object JsonManager {
         .disableHtmlEscaping()
         .create()
 
-    inline fun <reified T> readJsonFromFile(path: String, defaultValue: T? = null): T? {
+
+    inline fun <reified T> getObjectList(path: String): List<T?> {
+        PluginLogManager.i("type = ${T::class.java.simpleName}, file path = [$path]")
+        val jsonString = readJsonFromFile(path)
+        return if (jsonString.isNotEmpty()) {
+            mGson.fromJson(jsonString, (object : TypeToken<List<T>>() {}.type))
+        } else {
+            emptyList()
+        }
+    }
+
+    inline fun <reified T> getSingleObject(path: String, defaultValue: T): T {
+        PluginLogManager.i("type = ${T::class.java.simpleName}, file path = [$path]")
+        val jsonString = readJsonFromFile(path)
+        return if (jsonString.isNotEmpty()) {
+            mGson.fromJson(jsonString, T::class.java)
+        } else {
+            defaultValue
+        }
+    }
+
+    fun readJsonFromFile(path: String): String {
+        PluginLogManager.i("try to read json string from file [$path]")
         return try {
-            PluginLogManager.i("type = ${T::class.java.simpleName}, file path = [$path]")
             BufferedReader(InputStreamReader(FileInputStream(path), Charset.defaultCharset().name())).use {
                 val sb = StringBuilder()
                 var s = it.readLine()
@@ -25,16 +50,35 @@ object JsonManager {
                     sb.append(s)
                     s = it.readLine()
                 }
-                return if (sb.toString().isNotEmpty()) {
-                    mGson.fromJson(sb.toString(), T::class.java)
-                } else {
-                    defaultValue
-                }
+                return sb.toString()
             }
         } catch (e: Exception) {
             PluginLogManager.e("trying to read Json from file [$path] failed: ${e.localizedMessage}")
             e.printStackTrace()
-            defaultValue
+            ""
+        }
+    }
+
+    fun checkFileOrDirectoryAvailable(directory: File, file: PluginFile): Boolean {
+        PluginLogManager.i("checkFileOrDirectoryAvailable called: ${directory.path}")
+        return try {
+            if (directory.isDirectory && !directory.exists()) {
+                directory.mkdir()
+            }
+            PluginLogManager.i("directory [$directory] exists: ${directory.exists()}")
+            val f = File(directory, file.fileName)
+            PluginLogManager.i("file [$f] exists: ${f.exists()}")
+            if (directory.exists() && !f.exists()) {
+                javaClass.getResourceAsStream("/${file.fileName}")?.let {
+                    val result = Files.copy(it, f.toPath())
+                    PluginLogManager.i("file copy result: $result")
+                }
+            }
+            f.exists()
+        } catch (e: Exception) {
+            PluginLogManager.e("failed to check or copy file: ${e.localizedMessage}")
+            e.printStackTrace()
+            false
         }
     }
 }
---
+++ src/main/java/games/dripdrop/simustock/utils/UniqueIDManager.kt
@@ -0,0 +1,24 @@
+package games.dripdrop.simustock.utils
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+object UniqueIDManager {
+
+    fun createStockCode(): String {
+        return StringBuilder(UUID.randomUUID().toString().substring(0, 4).uppercase())
+            .append(SimpleDateFormat("yyMMddmm", Locale.PRC).format(Date()))
+            .append("DD")
+            .toString()
+    }
+
+    fun createOrderNumber(): String {
+        return StringBuilder("ON")
+            .append(SimpleDateFormat("yyMMddHHmmssSSS", Locale.PRC).format(Date()))
+            .append("U")
+            .append(UUID.randomUUID().toString().substring(0, 4).uppercase())
+            .toString()
+    }
+
+    inline fun <reified T> createTableName(): String = "simustock_${T::class.java.simpleName.lowercase()}"
+}
