df3656f49396b7354d0eea1229a4f72b61a7459c
mac <DripDropMC@outlook.com>
2024-07-12 20:12:23 +0800

    refactor(resources): change or add some data classes

---
+++ src/main/java/games/dripdrop/simustock/bean/PluginFile.kt
@@ -0,0 +1,7 @@
+package games.dripdrop.simustock.bean
+
+enum class PluginFile(val fileName: String) {
+    CONFIG_FILE("config.json"),
+    COMPANY_LIST_FILE("companies.json"),
+    DATABASE_FILE("simustock.db")
+}
