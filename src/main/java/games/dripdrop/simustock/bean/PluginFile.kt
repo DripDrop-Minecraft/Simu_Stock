077b25311dbe50517a4903d9555345ab78f90fcd
mac <DripDropMC@outlook.com>
2024-07-13 22:04:29 +0800

    feat(bean): add localization

--- src/main/java/games/dripdrop/simustock/bean/PluginFile.kt
+++ src/main/java/games/dripdrop/simustock/bean/PluginFile.kt
@@ -3,5 +3,6 @@ package games.dripdrop.simustock.bean
 enum class PluginFile(val fileName: String) {
     CONFIG_FILE("config.json"),
     COMPANY_LIST_FILE("companies.json"),
+    LOCALIZATION_FILE("localization.json"),
     DATABASE_FILE("simustock.db")
 }
