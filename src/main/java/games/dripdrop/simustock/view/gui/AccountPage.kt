b4a7dfc7d6b417852a4aa2208d5eea84f30f9104
mac <DripDropMC@outlook.com>
2024-07-23 22:19:33 +0800

    feat(gui): add some basic actions for 2 pages

--- src/main/java/games/dripdrop/simustock/view/gui/AccountPage.kt
+++ src/main/java/games/dripdrop/simustock/view/gui/AccountPage.kt
@@ -1,10 +1,20 @@
 package games.dripdrop.simustock.view.gui
 
 import games.dripdrop.simustock.presenter.interfaces.AbstractGuiManager
+import org.bukkit.entity.HumanEntity
 import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.inventory.InventoryType
 
 class AccountPage : AbstractGuiManager() {
+
     override fun onItemClicked(event: InventoryClickEvent) {
         TODO("Not yet implemented")
+    }
+
+    override fun initView(player: HumanEntity) {
+        createTypedInventory(InventoryType.HOPPER, getLocalization().titleOfMyAccount).apply {
+            // TODO
+            player.openInventory(this)
+        }
     }
 }
